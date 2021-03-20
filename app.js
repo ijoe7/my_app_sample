35d256ed28b75c9dbcbce490509a2e47734b1828
Oluwasegun Iyanda <iyandaoluwasegun7@gmail.com>
2021-03-20 21:52:47 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var more = 7
+var less = 2
+if (more > less){
+  result = "You have more than enough"
+}else{
+  result = "Be satisfied with what you have"
+}
+console.log(result)
---
+++ index.py
@@ -0,0 +1,6 @@
+more = int(input("Type in your largest number: "))
+less = int(input("Type in your lowest number: "))
+if more > less:
+    print("You have more than enough")
+else:
+    print("Be satisfied with what you have")
